7d208137f6515fda38419ac7cde7228a60b20094
Paul P <bigpaulie25ro@yahoo.com>
2017-04-28 22:31:49 +0300

    Adding Cacheabe trait, modifying README.md

---
+++ Doctrine/Support/Cacheable.php
@@ -0,0 +1,170 @@
+<?php
+
+namespace bigpaulie\CacheBundle\Doctrine\Support;
+
+use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\NoResultException;
+use Doctrine\ORM\Query;
+
+trait Cacheable
+{
+
+    /**
+     * Finds an Entity by its identifier.
+     *
+     * @param $id
+     * @param int|null $lifetime
+     * @param \Closure|null $callable
+     * @return \Closure|object|null
+     * @throws NoResultException
+     */
+    public function find($id, $lifetime = null, \Closure $callable = null)
+    {
+        $queryBuilder = $this->_em->createQueryBuilder();
+        $queryBuilder->select($this->_entityName)
+            ->from($this->_entityName, $this->_entityName);
+        $queryBuilder->where($this->_entityName . ".id = :id")->setParameter(':id', $id);
+        $queryBuilder->setMaxResults(1);
+
+        $query = $queryBuilder->getQuery();
+
+        /**
+         * If a lifetime is specified than we can cache
+         * otherwise skip caching.
+         */
+        if ( $lifetime !== null ) {
+            $cacheID = $this->cacheId($this->_entityName, $id);
+            $query->useResultCache(true, $lifetime, $cacheID);
+        }
+
+        /**
+         * If no result is found and $callable is not null
+         * that we can call the $callable at the end of the query
+         * $callable will act as a default return for the method.
+         */
+        if ( $callable !== null ) {
+            $result = null;
+            try {
+
+                $result = $query->getSingleResult(Query::HYDRATE_OBJECT);
+            } catch (NoResultException $exception) {}
+
+            return $result ? $result : $callable();
+        }
+
+        return $query->getSingleResult(Query::HYDRATE_OBJECT);
+    }
+
+    /**
+     * Finds a single entity by a set of criteria.
+     *
+     * @param array $criteria
+     * @param array|null $orderBy
+     * @param int|null $lifetime
+     * @param \Closure|null $callable
+     * @return object|null
+     * @throws NoResultException
+     */
+    public function findOneBy(array $criteria, array $orderBy = null, $lifetime = null, \Closure $callable = null)
+    {
+        $queryBuilder = $this->_em->createQueryBuilder();
+        $queryBuilder->select($this->_entityName)
+            ->from($this->_entityName, $this->_entityName);
+
+        foreach ($criteria as $field => $value) {
+            $queryBuilder->andWhere($this->_entityName . ".{$field} = :{$field}")
+                ->setParameter($field, $value);
+        }
+
+        if ( is_array($orderBy) ) {
+            foreach ($orderBy as $field => $direction) {
+                $queryBuilder->addOrderBy($field, $direction);
+            }
+        }
+
+        $queryBuilder->setMaxResults(1);
+
+        $query = $queryBuilder->getQuery();
+
+        /**
+         * If a lifetime is specified than we can cache
+         * otherwise skip caching.
+         */
+        if ( $lifetime ) {
+            $cacheID = $this->cacheId($this->_entityName, serialize($criteria));
+            $query->useResultCache(true, $lifetime, $cacheID);
+        }
+
+        /**
+         * If no result is found and $callable is not null
+         * that we can call the $callable at the end of the query
+         * $callable will act as a default return for the method.
+         */
+        if ( $callable !== null ) {
+            $result = null;
+            try {
+
+                $result = $query->getSingleResult(Query::HYDRATE_OBJECT);
+            } catch (NoResultException $exception) {}
+
+            return $result ? $result : $callable();
+        }
+
+        return $query->getSingleResult(Query::HYDRATE_OBJECT);
+    }
+
+    /**
+     * Finds all entities in the repository.
+     *
+     * @param null $lifetime
+     * @param \Closure|null $callable
+     * @return mixed|null
+     * @throws NoResultException
+     */
+    public function findAll($lifetime = null, \Closure $callable = null)
+    {
+        $queryBuilder = $this->_em->createQueryBuilder();
+        $queryBuilder->select($this->_entityName)
+            ->from($this->_entityName, $this->_entityName);
+
+        $query = $queryBuilder->getQuery();
+
+        /**
+         * If a lifetime is specified than we can cache
+         * otherwise skip caching.
+         */
+        if ( $lifetime !== null ) {
+            $cacheID = $this->cacheId($this->_entityName);
+            $query->useResultCache(true, $lifetime, $cacheID);
+        }
+
+        /**
+         * If no result is found and $callable is not null
+         * that we can call the $callable at the end of the query
+         * $callable will act as a default return for the method.
+         */
+        if ( $callable !== null ) {
+            $result = null;
+            try {
+
+                $result = $query->getResult(Query::HYDRATE_OBJECT);
+            } catch (NoResultException $exception) {}
+
+            return $result ? $result : $callable();
+        }
+
+        return $query->getResult(Query::HYDRATE_OBJECT);
+    }
+
+    /**
+     * Generate a cache id from a predefined key and a unique salt.
+     *
+     * @param string $key
+     * @param string $unique
+     * @return string
+     */
+    protected function cacheId($key, $unique = "")
+    {
+        return md5($key . $unique);
+    }
+}
---
+++ Entity/TestEntity.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace bigpaulie\CacheBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * TestEntity
+ */
+class TestEntity
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $firstName;
+
+    /**
+     * @var string
+     */
+    private $lastName;
+
+    /**
+     * @var int
+     */
+    private $age;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set firstName
+     *
+     * @param string $firstName
+     * @return TestEntity
+     */
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    /**
+     * Get firstName
+     *
+     * @return string 
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * Set lastName
+     *
+     * @param string $lastName
+     * @return TestEntity
+     */
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    /**
+     * Get lastName
+     *
+     * @return string 
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * Set age
+     *
+     * @param integer $age
+     * @return TestEntity
+     */
+    public function setAge($age)
+    {
+        $this->age = $age;
+
+        return $this;
+    }
+
+    /**
+     * Get age
+     *
+     * @return integer 
+     */
+    public function getAge()
+    {
+        return $this->age;
+    }
+}
